523bea471ebd282776615a9da7ff28afc28b428e
diodonfrost <diodon.frost@diodonfrost.me>
2018-10-24 12:35:06 +0200

    install atom modules with apm

--- manifests/packages/atom.pp
+++ manifests/packages/atom.pp
@@ -1,10 +1,23 @@
 # Install text editor atom
+class ubuntu_laptop::packages::atom (
+  Array $atom_packages = $::ubuntu_laptop::params::atom_packages,
 
-class ubuntu_laptop::packages::atom {
-
+  ){
   # Install  Atom with snap
   package {'atom':
     ensure   => installed,
     provider => snap,
+  }
+
+  # Install Atom packages
+  split($users, ',').each | $user | {
+    $atom_packages.each | $apm_package | {
+      exec {"Install atom package ${apm_package} for user ${user}":
+        command => "/snap/bin/apm install ${apm_package}",
+        user    => $user,
+        creates => "/home/${user}/.atom/packages/${apm_package}",
+        require => Package['atom'],
+      }
+    }
   }
 }
