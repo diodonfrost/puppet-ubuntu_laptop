0330f780ce783718b136e5a8406171b74457d895
diodonfrost <diodon.frost@diodonfrost.me>
2018-10-24 18:27:33 +0200

    add oh-my-zsh installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class ubuntu_laptop inherits ::ubuntu_laptop::params {
 
   include ::snapd
   include ubuntu_laptop::packages::install
+  include ubuntu_laptop::shell
 
   Class['snapd']
   -> Class['ubuntu_laptop::packages::install']
