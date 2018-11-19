a14516120f5eb070bb29eb0c1dc204b98564a791
diodonfrost <diodon.frost@diodonfrost.me>
2018-11-19 11:11:16 +0100

    update vagrant to new version

--- manifests/packages/vagrant.pp
+++ manifests/packages/vagrant.pp
@@ -4,6 +4,6 @@ class ubuntu_laptop::packages::vagrant {
   # Install Vagrant
   package {'vagrant':
     ensure => installed,
-    source => 'https://releases.hashicorp.com/vagrant/2.1.5/vagrant_2.1.5_x86_64.deb',
+    source => 'https://releases.hashicorp.com/vagrant/2.2.1/vagrant_2.2.1_x86_64.deb',
   }
 }
