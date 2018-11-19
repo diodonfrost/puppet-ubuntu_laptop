e55025bace5495bc30d8d82d0f074d1cc8b276c0
diodonfrost <diodon.frost@diodonfrost.me>
2018-11-19 11:42:18 +0100

    fix packer extract

--- manifests/packages/packer.pp
+++ manifests/packages/packer.pp
@@ -4,10 +4,12 @@ class ubuntu_laptop::packages::packer {
   # Install Vagrant
   archive {'packer_1.3.2_linux_amd64.zip':
     ensure       => present,
+    path         => '/tmp/packer_1.3.2_linux_amd64.zip',
     extract      => true,
     source       => 'https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip',
     extract_path => '/usr/local/bin',
     cleanup      => true,
     creates      => '/usr/local/bin/packer',
+    require      => Package['unzip'],
   }
 }
